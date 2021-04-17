30613b3427d7816557a127e1b3159bcc1c991552
Onizuka <51335422+sekibura@users.noreply.github.com>
2021-04-17 10:11:49 +0300

    start lab4

---
+++ Lab4/Source.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int* argc, char **argv) {
+	int numtasks, rank;
+	
+	MPI_Init(argc, &argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+	printf("Hello MPI from =%d, total n of prc %d\n", rank, numtasks);
+	MPI_Finalize();
+}
